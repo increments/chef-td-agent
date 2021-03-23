7ca897adcc2bf9b7987abed4b6f3293b7ceddacb
nagaya <nagaya@a-tm.co.jp>
2021-03-23 16:37:55 +0900

    Add provides for chef16

--- libraries/resource_td_agent_gem.rb
+++ libraries/resource_td_agent_gem.rb
@@ -3,6 +3,8 @@ require 'chef/resource'
 class Chef
   class Resource
     class TdAgentGem < Chef::Resource::GemPackage
+      provides :td_agent_gem
+
       def initialize(*args)
         super
         @action = :install
